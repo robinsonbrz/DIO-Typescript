806d20bbaa9270c03ab6782cd5c06a6b8f8fb1a7
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-06-12 14:44:58 -0300

    😂 Lidando com objeto, enum interface

--- app.ts
+++ app.ts
@@ -1,23 +1,70 @@
-let button = document.getElementById('button');
-let input1 = document.getElementById('input1') as HTMLInputElement;
-let input2 = document.getElementById('input2') as HTMLInputElement;
+const pessoa = {
+    nome: 'Mariana',
+    idade: 28,
+    profissao: 'desenvolvedora',
+}
 
-function adicionarNumero(num1: number, num2: number, devePrintar:boolean, frase: string) {
-    let resultado = num1 + num2;
-    if(devePrintar){
-        console.log(frase + resultado)
+pessoa.idade = 25;
+
+const andre: {nome: string, idade: number, profissão: string} = {
+    nome: 'Andre',
+    idade: 25,
+    profissão: 'pintor'
+}
+
+const paula: {nome: string, idade: number, profissão: string} = {
+    nome: 'Paula',
+    idade: 25,
+    profissão: 'Desenvolvedora'
+}
+
+enum Profissao {
+    Professora,
+    Atriz,
+    Desenvolvedora,
+    JogadoraDeFutebol
+}
+
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: Profissao
+}
+
+interface Estudante extends Pessoa {
+    materias: string[]
+}
+
+const vanessa: Pessoa = {
+    nome: 'Vanessa',
+    idade: 23,
+    profissao: Profissao.Desenvolvedora
+}
+
+const maria: Pessoa = {
+    nome: 'Maria',
+    idade: 23,
+    profissao: Profissao.Professora
+}
+
+const jessica: Estudante = {
+    nome: 'Jessica',
+    idade: 28,
+    profissao: Profissao.Desenvolvedora,
+    materias: ['Matemática discreta', 'programação']
+}
+
+const monica: Estudante = {
+    nome: 'monica',
+    idade: 28,
+    // Em estudante não é obrigatório profissão devido ao ?
+    materias: ['Matemática discreta', 'programação']
+}
+
+function listar(lista: string[]){
+    for (const item of lista){
+        console.log('-' + item);
     }
-    return num1 + num2;
 }
 
-let devePrintar = true;
-let frase = 'O valor é: '
-
-if (button){
-    button.addEventListener('click', () => {
-        if (input1 && input2){
-            console.log(adicionarNumero(Number(input1.value), Number(input2.value),devePrintar, frase));
-        }
-    })
-}
-
+listar(monica.materias)
