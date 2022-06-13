d9abbf0c129b05a6bc3ca208f2a9d35f75014c4a
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-06-13 15:15:43 -0300

    🤣 Definindo tipo de retorno da funcao

--- app.ts
+++ app.ts
@@ -1,12 +1,4 @@
-type input = number | string;
-
-function somarValores(input1: input, input2: input) {
-    if (typeof input1 === 'string' ||typeof input2 === 'string'){
-        return input1.toString() + input2.toString();
-    } else {
-        return input1 + input2;
-    }
+function somarValoresNumericos(numero1: number, numero2: number): number {
+    return numero1 + numero2;
 }
-console.log(somarValores(1, 5));
-console.log(somarValores('ola', ', tudo bem!'));
-console.log(somarValores('Que dia', 5));
+console.log(somarValoresNumericos(1,2));
