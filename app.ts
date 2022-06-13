f6e6c316c768123bf1226ec4a8d1e4338fcd7c85
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-06-13 15:20:46 -0300

    😊 Retorno void

--- app.ts
+++ app.ts
@@ -2,3 +2,9 @@ function somarValoresNumericos(numero1: number, numero2: number): number {
     return numero1 + numero2;
 }
 console.log(somarValoresNumericos(1,2));
+
+function printaValoresNumericos(numero1: number, numero2: number): void {
+    console.log(numero1 + numero2);
+}
+
+printaValoresNumericos(3,6);
