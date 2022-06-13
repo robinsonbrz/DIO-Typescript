9d77eefca0010d4ffd8ae7d79a410ab39bada2f0
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-06-13 16:14:54 -0300

    💕 tipo unknown

--- app.ts
+++ app.ts
@@ -1,20 +1,24 @@
-function printaValoresNumericos(numero1: number, numero2: number): void {
-    console.log(numero1 + numero2);
+let anyEstaDeVolta: any;
+anyEstaDeVolta = 3;
+anyEstaDeVolta = 'teste';
+anyEstaDeVolta = 5;
+
+let stringTest: string = 'verificar';
+stringTest = anyEstaDeVolta;
+
+let unknownValor: unknown;
+unknownValor = 3;
+unknownValor = 'opa';
+unknownValor = true;
+unknownValor = 'vai sim';
+
+let stringTest2: string = 'agora vai';
+// stringTest2 = unknownValor;
+
+if (typeof unknownValor === 'string'){
+    stringTest2 = unknownValor;
 }
 
-function somarValoresNumericosETratar(numero1: number, numero2: number, callback: (numero: number) => number): number {
-    let resultado = numero1 + numero2;
-    return callback(resultado);
-}
 
-function aoQuadrado(numero: number): number {
-    return numero * numero;
-}
 
-function dividirPorEleMesmo(number: number):number{
-    return number+2 / number
-}
-
-console.log(somarValoresNumericosETratar(1,3, aoQuadrado));
-console.log(somarValoresNumericosETratar(1,3, dividirPorEleMesmo));
 
