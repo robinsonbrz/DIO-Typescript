56ee6a4313f5e988a6be52b14e1639dd75dc50a7
ROBINSON ENEDINO E SILVA <robinsonbrz@gmail.com>
2022-06-13 16:32:59 -0300

    😘 never e trown

--- app.ts
+++ app.ts
@@ -19,6 +19,16 @@ if (typeof unknownValor === 'string'){
     stringTest2 = unknownValor;
 }
 
+function jogaErro(erro: string, codigo: number): never {
+    throw {
+        error: erro,
+        code: codigo
+    }
+}
+
+jogaErro('deu erro', 500 );
+
+
 
 
 
